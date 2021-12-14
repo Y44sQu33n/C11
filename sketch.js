860f8ada1723414f8020fc19a02e6dfb868e94fc
Y44sQu33n <94427572+Y44sQu33n@users.noreply.github.com>
2021-12-13 18:58:40 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var trex, trex_running, edges;
+var groundImage;
+var ground;
+var invisiground
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  groundImage = loadImage("ground2.png")
+}
+
+function setup(){
+  createCanvas(600,200);
+  
+  // creating trex
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  edges = createEdgeSprites();
+  
+  //adding scale and position to trex
+  trex.scale = 0.5;
+  trex.x = 50
+
+  ground = createSprite(300,180,1200,15);
+  ground.addImage(groundImage);
+  ground.velocityX = -5
+
+  invisiground = createSprite(300,190,1200,15)
+  invisiground.visible = false
+}
+
+
+function draw(){
+  //set background color 
+  background("white");
+  
+  //logging the y position of the trex
+  console.log(trex.y)
+  
+  //jump when space key is pressed
+  if(keyDown("space") && trex.y >= 159){
+    trex.velocityY = -10;
+
+  }
+  
+  trex.velocityY = trex.velocityY + 0.5;
+  
+  //stop trex from falling down
+  trex.collide(invisiground);
+  drawSprites();
+
+  if(ground.x < 0) {
+    ground.x = ground.width/2
+  }
+
+}
